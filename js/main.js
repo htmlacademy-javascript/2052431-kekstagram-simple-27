b241b7bf1971ff97ab93c94878f4d4dadd5ce45c
Dariamoys <dash2004@inbox.ru>
2022-11-22 10:25:07 +0300

    Добавляет запросы на сервер, сообщения об ошибках

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-import {createPhotoDataArr} from './data.js';
-import './render-thumbnail.js';
-import './upload-form.js';
-import './image-resize.js';
-import './effect.js';
-console.log(createPhotoDataArr());
+// import './render-thumbnail.js';
+// import './upload-form.js';
+// import './image-resize.js';
+// import './effect.js';
+// import './api.js';
+// import './alerts.js';
+import {RenderSimilarthumbnails} from './render-thumbnail.js';
+import {setUserFormSubmit, closeModal} from './upload-form.js';
+import {getData} from './api.js';
+import {showAlert} from './util.js';
+
+getData(RenderSimilarthumbnails, showAlert);
+setUserFormSubmit(closeModal);
+
 
